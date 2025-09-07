d4529b8c0c50bb16ca4f24559a6c89671649943b
xingmiye <66361330+xingmiye@users.noreply.github.com>
2025-09-07 17:02:24 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -5,7 +5,7 @@ source shell/custom-packages.sh
 #echo "✅ 你选择了第三方软件包：$CUSTOM_PACKAGES"
 # 下载 run 文件仓库
 echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
-git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
+git clone -b lwdev --depth=1 https://github.com/xingmiye/customstore.git /tmp/store-run-repo
 
 # 拷贝 run/arm64 下所有 run 文件和ipk文件 到 extra-packages 目录
 mkdir -p /home/build/immortalwrt/extra-packages
